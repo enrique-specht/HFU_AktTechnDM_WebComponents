a71b27be8c11eb7778b415a8670d7d24b4a9ac64
enrique-specht <enrique.michael.specht@hs-furtwangen.de>
2024-11-16 14:56:47 +0100

    weather widget

--- angular-web-components/src/components/weather-widget/weather-widget.component.ts
+++ angular-web-components/src/components/weather-widget/weather-widget.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { fetchWeatherApi } from 'openmeteo';
+import { firstValueFrom } from 'rxjs';
+
+type currentWeather = {
+  time?: Date;
+  temperature2m?: number;
+  relativeHumidity2m?: number;
+  precipitation?: number;
+  weatherCode?: number;
+  windSpeed10m?: number;
+};
+
+type forecastWeather = {
+  time?: Date;
+  weatherCode?: number;
+  temperature2mMax?: number;
+  temperature2mMin?: number;
+}[];
 
 @Component({
   selector: 'app-weather-widget',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './weather-widget.component.html',
   styleUrl: './weather-widget.component.scss',
 })
-export class WeatherWidgetComponent {}
+export class WeatherWidgetComponent implements OnInit {
+  private forecastDays = 7;
+  private autoReloadInMs = 10 * 60 * 1000; // 10 min
+  private locationLatLong = [48.0516, 8.2072];
+  protected currentWeather: currentWeather = {};
+  protected forecastWeather: forecastWeather = [];
+  protected dateToday = new Date();
+  protected locationTimezone = 'CET';
+  protected weatherCodes: any;
+
+  constructor(
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.weatherApi();
+    setInterval(() => {
+      this.weatherApi();
+    }, this.autoReloadInMs);
+
+    firstValueFrom(this.httpClient.get('./weathercode_icons.json')).then(
+      (weathercode) => {
+        this.weatherCodes = weathercode;
+      }
+    );
+  }
+
+  async weatherApi(): Promise<void> {
+    const range = (start: number, stop: number, step: number) =>
+      Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+    const params = {
+      latitude: this.locationLatLong[0],
+      longitude: this.locationLatLong[1],
+      current: [
+        'temperature_2m',
+        'relative_humidity_2m',
+        'precipitation',
+        'weather_code',
+        'wind_speed_10m',
+      ],
+      daily: ['weather_code', 'temperature_2m_max', 'temperature_2m_min'],
+      forecast_days: this.forecastDays,
+      timeformat: 'unixtime',
+      timezone: 'auto',
+    };
+    const url = 'https://api.open-meteo.com/v1/forecast';
+
+    const responses = await fetchWeatherApi(url, params);
+    const response = responses[0];
+
+    const current = response.current()!;
+    const daily = response.daily()!;
+
+    this.locationTimezone = response.timezoneAbbreviation()!;
+
+    this.currentWeather = {
+      time: new Date(Number(current.time()) * 1000),
+      temperature2m: current.variables(0)!.value(),
+      relativeHumidity2m: current.variables(1)!.value(),
+      precipitation: current.variables(2)!.value(),
+      weatherCode: current.variables(3)!.value(),
+      windSpeed10m: current.variables(4)!.value(),
+    };
+
+    this.forecastWeather = [];
+    for (let index = 0; index < this.forecastDays; index++) {
+      this.forecastWeather.push({
+        time: range(
+          Number(daily.time()),
+          Number(daily.timeEnd()),
+          daily.interval()
+        ).map((t) => new Date(t * 1000))[index],
+        weatherCode: daily.variables(0)!.valuesArray()![index],
+        temperature2mMax: daily.variables(1)!.valuesArray()![index],
+        temperature2mMin: daily.variables(2)!.valuesArray()![index],
+      });
+    }
+
+    this.changeDetectorRef.detectChanges();
+
+    console.log('Updated current weather:', this.currentWeather);
+    console.log('Updated forecast weather:', this.forecastWeather);
+  }
+}
--- angular-web-components/src/main.ts
+++ angular-web-components/src/main.ts
@@ -1,6 +1,11 @@
 import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
-import { Type } from '@angular/core';
+import { LOCALE_ID, Type } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 /* Import polyfills here instead of angular.json */
 import 'zone.js';
@@ -11,7 +16,9 @@ const webComponents: [[Type<any>, string]] = [
   [WeatherWidgetComponent, 'weather-widget'],
 ];
 
-createApplication()
+createApplication({
+  providers: [provideHttpClient(), { provide: LOCALE_ID, useValue: 'de-de' }],
+})
   .then((app) => {
     webComponents.forEach(([type, selector]) => {
       const ngElement = createCustomElement(type, {
